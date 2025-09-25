82c77bb52990f75241279a9d592b8ac057eb6b56
Adanankhan <adnanp3799@gmail.com>
2025-09-25 19:06:45 +0530

    Step 14: Implement custom delimiter support

--- string-calculator-tdd/src/main/java/com/incubyte/calculator/Calculator.java
+++ string-calculator-tdd/src/main/java/com/incubyte/calculator/Calculator.java
@@ -1,12 +1,21 @@
 package com.incubyte.calculator;
 
+import java.util.regex.Pattern;
+
 public class Calculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
         }
-        numbers = numbers.replace("\n", ",");
-        String[] parts = numbers.split(",");
+
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterEnd = numbers.indexOf("\n");
+            delimiter = Pattern.quote(numbers.substring(2, delimiterEnd));
+            numbers = numbers.substring(delimiterEnd + 1);
+        }
+
+        String[] parts = numbers.split(delimiter);
         int sum = 0;
         for (String num : parts) {
             sum += Integer.parseInt(num);
