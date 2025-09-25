76834cafce4130f476049c1d15a17b16c6d395e7
Adanankhan <adnanp3799@gmail.com>
2025-09-25 18:25:30 +0530

    Step 1: Created package, Calculator class, and first failing test

---
+++ string-calculator-tdd/src/main/java/com/incubyte/calculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.incubyte.calculator;
+
+public class Calculator {
+    public int add(String numbers){
+        return 1;
+    }
+}
+
---
+++ string-calculator-tdd/src/test/java/com/incubyte/calculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.incubyte.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Test
+    void emptyStringReturnsZero(){
+        Calculator calculator = new Calculator();
+        assertEquals(0, calculator.add(""));
+    }
+}
