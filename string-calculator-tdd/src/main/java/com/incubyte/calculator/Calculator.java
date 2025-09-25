77d4a71b21691e8bd0092018cf64ab5f0e94911e
Adanankhan <adnanp3799@gmail.com>
2025-09-25 18:35:19 +0530

    Step 3: Implement add() to pass empty string test

--- string-calculator-tdd/src/main/java/com/incubyte/calculator/Calculator.java
+++ string-calculator-tdd/src/main/java/com/incubyte/calculator/Calculator.java
@@ -2,7 +2,10 @@ package com.incubyte.calculator;
 
 public class Calculator {
     public int add(String numbers){
-        return 0;
+        if(numbers.isEmpty()){
+            return 0;
+        }
+        return 1;
     }
 }
 
