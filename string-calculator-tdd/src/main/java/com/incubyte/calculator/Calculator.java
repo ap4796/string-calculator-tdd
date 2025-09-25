3b1ee5f1fd9154b8e260250a092f88f3c0ffca28
Adanankhan <adnanp3799@gmail.com>
2025-09-25 18:49:38 +0530

    Step 7: Implement parsing and sum for two comma-separated numbers

--- string-calculator-tdd/src/main/java/com/incubyte/calculator/Calculator.java
+++ string-calculator-tdd/src/main/java/com/incubyte/calculator/Calculator.java
@@ -2,8 +2,12 @@ package com.incubyte.calculator;
 
 public class Calculator {
     public int add(String numbers){
-        if(numbers.isEmpty()){
+        if (numbers.isEmpty()){
             return 0;
+        }
+        if (numbers.contains(",")){
+            String [] parts = numbers.split(",");
+            return Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
         }
         return Integer.parseInt(numbers);
     }
